9484a6d45c8d3f85f8ee03dbc4950b59043ac5fb
Android <Android99@outlook.com>
2024-08-11 21:34:19 +0800

    本地化搜索

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,9 +13,7 @@ export default defineConfig({
       { text: '海豹', link: 'https://dice.weizaima.com/' },
       { text: '塞壬唱片', link: 'https://monster-siren.hypergryph.com/' }
     ],
-    search: {
-      provider: 'local'
-    },
+
     sidebar: {
       '/sealdice/': [
         {
@@ -81,6 +79,34 @@ export default defineConfig({
       formatOptions: {
         dateStyle: 'short',
         timeStyle: 'medium'
+      }
+    },
+
+    search: {
+      provider: 'local',
+      options: {
+        detailedView: true,
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            displayDetails: '显示详细列表',
+            resetButtonTitle: '清除查询条件',
+            backButtonTitle: '关闭搜索',
+            noResultsText: '无法找到相关结果',
+            footer: {
+              selectText: '选择',
+              selectKeyAriaLabel: '输入',
+              navigateText: '切换',
+              navigateUpKeyAriaLabel: '上箭头',
+              navigateDownKeyAriaLabel: '下箭头',
+              closeText: '关闭',
+              closeKeyAriaLabel: 'esc'
+            }
+          }
+        }
       }
     },
 
